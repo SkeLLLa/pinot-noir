21e0235ce33b0887b537b3348b16a4b047e6673f
Commit User <code.commit.user@gmail.com>
2024-07-16 14:11:22 +0200

    fix: add error export

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export * from './utils/format';
 export * from './utils/tag';
 export * from './client/clients';
 export * from './client/broker/transport/transports';
+export * from './client/errors/pinot';
