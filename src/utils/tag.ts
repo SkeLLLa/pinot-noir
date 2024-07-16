e461b333fc6793d2ed04ee7a43c92c00ba8dbe67
Commit User <code.commit.user@gmail.com>
2024-07-16 15:12:41 +0200

    fix: sql tag export

--- src/utils/tag.ts
+++ src/utils/tag.ts
@@ -1,1 +1,10 @@
+import { Sql } from 'sql-template-tag';
+
 export * from 'sql-template-tag';
+
+export const sql = (
+  strings: readonly string[],
+  ...values: readonly unknown[]
+) => {
+  return new Sql(strings, values);
+};
