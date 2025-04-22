556297c505a40c95c0f325213b7bda93661ea78d
Commit User <code.commit.user@gmail.com>
2025-04-22 16:41:02 +0200

    feat: add missing query options

--- src/client/broker/types.ts
+++ src/client/broker/types.ts
@@ -269,13 +269,22 @@ export interface IPinotQueryOptions {
   /** Minimum groups to keep when trimming groups at the server level for group-by queries. */
   minServerGroupTrimSize?: number;
 
-  /** Which indexes to skip usage of, per-column. Format: `col1=indexType1,indexType2&col2=indexType1`. */
+  /**
+   * Which indexes to skip usage of (i.e. scan instead), per-column. This is useful for side-by-side comparison/debugging. There can be cases where the use of an index is actually more expensive than performing a scan of the docs which match other filters. One such example could be a low-selectivity inverted index used in conjunction with another highly selective filter.
+   * Config can be specified using url parameter format: skipIndexes='col1=inverted,range&col2=inverted'.
+   * Possible index types to skip are: sorted, range, inverted, H3. To find out which indexes are used to resolve a given query, use the EXPLAIN query.
+   * @defaultValue `null`
+   **/
   skipIndexes?: string;
 
   /** For upsert-enabled table, skip the effect of upsert and query all records. */
   skipUpsert?: boolean;
 
-  /** Use star-tree index if available. (introduced in 0.11.0) */
+  /**
+   * Use star-tree index if available. (introduced in 0.11.0)
+   *
+   * @defaultValue `true`
+   **/
   useStarTree?: boolean;
 
   /** Enable scan reordering for AND clauses. */
@@ -284,7 +293,11 @@ export interface IPinotQueryOptions {
   /** Maximum rows allowed in join hash-table creation phase. */
   maxRowsInJoin?: number;
 
-  /** Indicates that the values in the IN clause are already sorted. */
+  /**
+   * Indicates that the values in the IN clause are already sorted.
+   *
+   * @defaultValue `false`
+   **/
   inPredicatePreSorted?: boolean;
 
   /** Algorithm to use to look up the dictionary ids for the IN clause values. */
@@ -312,6 +325,41 @@ export interface IPinotQueryOptions {
    * @defaultValue 1
    */
   queueTolerance?: TQueueTolerancePredefined | number;
+  /**
+   * For aggregation and group-by queries, ask servers to directly return final results instead of intermediate results for aggregations.
+   * Can be applied when the group key is server partitioned, i.e. the column(s) is partitioned, and all the data for a partition is served by the same server.
+   */
+  serverReturnFinalResult?: boolean;
+  /**
+   * For group-by queries, ask servers to directly return final results instead of intermediate results for aggregations.
+   * Different from serverReturnFinalResult, this option should be used when the group key is not server partitioned, but the aggregated column is server partitioned. It is particularly useful for distinct count queries.
+   * When this option is enabled, server will return final results, but won't directly trim the result to the query limit.
+   *
+   * @defaultValue false
+   */
+  serverReturnFinalResultKeyUnpartitioned?: boolean;
+
+  /**
+   * This config can be set to true to avoid computing all the groups in a group by query with only filtered aggregations (and no non-filtered aggregations). By default, the groups are computed over all the rows returned by the main filter, even if certain rows will never match any of the aggregation filters. This is the standard SQL behavior. However, if the selectivity of the main filter is very high as compared to the selectivity of the aggregation filters, this query option can help provide a big performance boost if the empty groups aren't required. For instance, a query like SELECT SUM(X) FILTER (WHERE Y = 1) FROM mytable will compute the groups over all the rows in the table by default since there's no main query filter. Setting this query option to true in such cases can massively improve performance if there's an inverted index on column Y for instance.
+   *
+   * @defaultValue false
+   */
+  filteredAggregationsSkipEmptyGroups?: boolean;
+
+  /**
+   * Set dropResults=true in the config to drop the resultTable from the response.
+   * Use this option to troubleshoot a customer's query (which may have sensitive data in the result) using metadata only.
+   *
+   * @defaultValue false
+   */
+  dropResults?: boolean;
+
+  /**
+   * Set skipUnavailableServers=true in the config to continue sending queries to remaining servers if dispatching a query fails.
+   *
+   * @defaultValue false
+   */
+  skipUnavailableServers?: boolean;
 }
 
 /**
